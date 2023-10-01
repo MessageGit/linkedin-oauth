75f0705c675d297aae68c32bb79f9c5f158b2357
MessageBox <message-box@outlook.fr>
2023-10-01 15:38:41 +0200

    feat(init): init. of server

---
+++ Api/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`\n\n\x1b[34m > 🚀 Server is running on: ${await app.getUrl()}`);
+}
+
+bootstrap();
