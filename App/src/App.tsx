aec7b5a6c8b07b7d03d8f518a780c8ead9d87717
MessageBox <message-box@outlook.fr>
2023-10-02 11:00:03 +0200

    feat(app): display of linkedin user data + loading spinner to handle the request loading

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,10 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import API from './api/api';
 import './App.css';
 
 import LinkedInIcon from './assets/linkedin_icon.png';
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [profileImageUrl, setProfileImageUrl] = useState<string>('');
+
   /* Load LinkedIn credentials from dotenv */
   const LINKEDIN_ENV = {
     CLIENT_ID: process.env['REACT_APP_LINKEDIN_CLIENT_ID'],
@@ -22,20 +27,46 @@ function App() {
   useEffect(() => {
     if (authorizationCode) {
       // TODO: Send authorization code to server for validity check
+      setIsLoading(true);
       API.post('linkedin/auth/login', { authorization_code: authorizationCode })
-        .then((res) => console.log('res', res))
-        .catch((err) => console.log('err', err));
+        .then((res) => {
+          const { firstName, lastName, email, profileImageUrl } = res.data || {};
+          setUsername(`${firstName} ${lastName}`);
+          setProfileImageUrl(profileImageUrl);
+          setEmail(email);
+        })
+        .catch((err) => console.log(err))
+        .finally(() => setIsLoading(false));
     }
   }, [authorizationCode]);
 
   return (
     <div className="App">
 
-      {/* Sign-in with LinkedIn - Button */}
-      <button className="LinkedIn_Button" onClick={onSignInLinkedIn}>
-        <img src={LinkedInIcon} alt="Logo LinkedIn" className="LinkedIn_Icon" />
-        <span>Se connecter via LinkedIn</span>
-      </button>
+      {isLoading ? (
+        <div className="Loading">
+          <div className="Progress_Bar">
+            <div className="Circle Border">
+            </div>
+          </div>
+          <span>Chargement en cours..</span>
+        </div>
+      ) :
+        email.length > 0 ? ( /* Current identity from LinkedIn */
+          <div className="Identity">
+            <i>Vous êtes connecté(e) à LinkedIn ✅<br />Voici les informations extraites de votre profil :</i>
+            <img src={profileImageUrl} className="Profile_Picture" alt={username} />
+            <div className="Text_Data">
+              <span className="Username">{username}</span>
+              <span>({email})</span>
+            </div>
+          </div>
+        ) : ( /* Sign-in with LinkedIn */
+          <button className="LinkedIn_Button" onClick={onSignInLinkedIn}>
+            <img src={LinkedInIcon} alt="Logo LinkedIn" className="LinkedIn_Icon" />
+            <span>Se connecter via LinkedIn</span>
+          </button>
+        )}
 
     </div>
   );
